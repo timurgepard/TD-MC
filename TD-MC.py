a923e7d6185d552c8dfb7a45b70690d34bcd33cf
timurgepard <timur-ishuov@rambler.ru>
2022-12-20 16:03:04 +0600

    Update TD-MC.py

--- TD-MC.py
+++ TD-MC.py
@@ -27,8 +27,8 @@ class DDPG():
                  batch_size =64, # batch size for training actor and critic networks
                  max_time_steps = 1000,# no of time steps per epoch
                  gamma  = 0.99,
-                 actor_learning_rate = 0.0001,
-                 critic_learning_rate = 0.001,
+                 actor_learning_rate = 0.0002,
+                 critic_learning_rate = 0.002,
                  n_episodes = 1000):# no of episodes to run
 
 
@@ -52,7 +52,7 @@ class DDPG():
         self.rewards_norm = divide_rewards_by
 
         self.tr_steps = round(1/self.eps)
-        self.n_steps = round(2/self.eps)
+        self.n_steps = round(4/self.eps)
         self.horizon = int(batch_size/2)+1 #+1 to fetch next state from current state roll_out
         self.max_steps = max_time_steps  ## Time limit for a episode
         self.replay = Replay(self.max_record_size, self.batch_size)
@@ -121,7 +121,7 @@ class DDPG():
     def eps_step(self):
         self.eps = math.exp(-self.x)
         self.tr_steps = round(1/self.eps)
-        self.n_steps = round(2/self.eps)
+        self.n_steps = round(4/self.eps)
         if self.n_steps<=self.horizon-1:
             self.x += 0.2*self.act_learning_rate
 
