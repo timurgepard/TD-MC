600d235e9e9001fa839bb9244d75d3f6951fd007
timurgepard <timur-ishuov@rambler.ru>
2022-12-21 15:13:28 +0600

    Update TD-MC.py

--- TD-MC.py
+++ TD-MC.py
@@ -1,3 +1,80 @@
+import tensorflow as tf
+import logging
+tf.get_logger().setLevel(logging.ERROR)
+
+from tensorflow.keras.optimizers import Adam, SGD
+from tensorflow.keras.initializers import RandomUniform as RU
+from tensorflow.keras.layers import Dense, Input, concatenate
+from tensorflow.keras import Model
+from tensorflow.keras import backend as K
+
+
+import random
+import numpy as np
+#import os
+#os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+import math
+import gym
+from collections import deque
+import math
+
+
+
+class Replay:
+    def __init__(self, max_buffer_size, batch_size):
+        self.max_buffer_size = max_buffer_size
+        self.batch_size = batch_size
+        self.buffer = deque(maxlen=max_buffer_size)
+
+    def add_experience(self, transition):
+        self.buffer.append(transition)
+
+    def sample_batch(self):
+        arr = np.array(random.sample(self.buffer, self.batch_size))
+        states_batch = np.vstack(arr[:, 0])
+        actions_batch = np.array(list(arr[:, 1]))
+        rewards_batch = np.vstack(arr[:, 2])
+        Q_batch = np.vstack(arr[:, 3])
+        next_states_batch = np.vstack(arr[:, 4])
+        Q_next_batch = np.vstack(arr[:, 5])
+
+        return states_batch, actions_batch, rewards_batch, Q_batch, next_states_batch, Q_next_batch
+
+def atanh(x):
+    return K.abs(x)*K.tanh(100*x)
+
+class _actor_network():
+    def __init__(self, state_dim, action_dim,action_bound_range=1):
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.action_bound_range = action_bound_range
+
+    def model(self):
+        state = Input(shape=self.state_dim, dtype='float64')
+        x = Dense(400, activation=atanh, kernel_initializer=RU(-1/np.sqrt(self.state_dim),1/np.sqrt(self.state_dim)))(state)
+        x = concatenate([state, x])
+        x = Dense(300, activation=atanh, kernel_initializer=RU(-1/np.sqrt(400+self.state_dim),1/np.sqrt(400+self.state_dim)))(x)
+        x = concatenate([state, x])
+        out = Dense(self.action_dim, activation='tanh',kernel_initializer=RU(-0.003,0.003))(x)
+        return Model(inputs=state, outputs=out)
+
+
+class _critic_network():
+    def __init__(self, state_dim, action_dim):
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+
+    def model(self):
+        state = Input(shape=self.state_dim, name='state_input', dtype='float64')
+        x = Dense(400, activation=atanh, kernel_initializer=RU(-1/np.sqrt(self.state_dim),1/np.sqrt(self.state_dim)))(state)
+        action = Input(shape=(self.action_dim,), name='action_input')
+        x = concatenate([x, state, action])
+        x = Dense(300, activation=atanh, kernel_initializer=RU(-1/np.sqrt(400+self.state_dim+self.action_dim),1/np.sqrt(400+self.state_dim+self.action_dim)))(x)
+        x = concatenate([x, state, action])
+        out = Dense(1, activation='linear')(x)
+        return Model(inputs=[state, action], outputs=out)
+
 class DDPG():
     def __init__(self,
                  env , # Gym environment with continous action space
