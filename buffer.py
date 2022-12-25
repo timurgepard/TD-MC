30a4e4c035b1c99bec83ff7cd5782adccfb7416c
timurgepard <timur-ishuov@rambler.ru>
2022-12-25 14:35:47 +0600

    Update buffer.py

--- buffer.py
+++ buffer.py
@@ -2,23 +2,22 @@ from collections import deque
 import random
 import numpy as np
 
+
 class Replay:
     def __init__(self, max_buffer_size, batch_size):
         self.max_buffer_size = max_buffer_size
         self.batch_size = batch_size
         self.buffer = deque(maxlen=max_buffer_size)
 
-    def add_experience(self, state, action, reward, Q, next_state, next_Q):
-        self.buffer.append([state, action, reward, Q, next_state, next_Q])
+    def add_experience(self, transition):
+        self.buffer.append(transition)
 
-    def sample_batch(self):
-        arr = np.array(random.sample(self.buffer, self.batch_size))
+
+    def sample(self):
+        arr = np.random.default_rng().choice(self.buffer, size=self.batch_size, replace=False)
         states_batch = np.vstack(arr[:, 0])
         actions_batch = np.array(list(arr[:, 1]))
         rewards_batch = np.vstack(arr[:, 2])
-        Q_batch = np.vstack(arr[:, 3])
-        next_states_batch = np.vstack(arr[:, 4])
-        Q_next_batch = np.vstack(arr[:, 5])
-
-        return states_batch, actions_batch, rewards_batch, Q_batch, next_states_batch, Q_next_batch
-
+        states_next = np.vstack(arr[:, 3])
+        done_batch = np.vstack(arr[:, 4])
+        return states_batch, actions_batch, rewards_batch, states_next, done_batch
